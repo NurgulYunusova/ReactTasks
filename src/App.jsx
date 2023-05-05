bf9d0c9bf5500f9e0eaceffb6a219fe6a8d2035e
Nurgul F. Yunusova <nurgulfn@code.local>
2023-05-05 19:03:13 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import UsersTable from "./components/UsersTable";
+
+function App() {
+  return (
+    <>
+      <UsersTable />
+    </>
+  );
+}
+
+export default App;
